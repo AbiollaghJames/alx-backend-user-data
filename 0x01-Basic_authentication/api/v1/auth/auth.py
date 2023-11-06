3485e51bb78d7fbe0b2276a510dbb60105fbec76
AbiollaghJames <abiollajames@protonmail.com>
2023-11-06 18:32:03 +0300

    Basc Authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """
+    manage the API authentication.
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Just returns False
+        """
+        return False
+    
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Just returns None
+        """
+        return None
+    
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Just returns None
+        """
+        return None
