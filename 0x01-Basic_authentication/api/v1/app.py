2403a91682e86d521863e8ccd08068bd1bf6117c
AbiollaghJames <abiollajames@protonmail.com>
2023-11-06 16:34:44 +0300

    Basic Authentication

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -21,13 +21,6 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
-@app.errorhandler
-def unathorized(401) -> str:
-    """Uauthorized request
-    """
-    return jsonify({"error": "Unauthorized"}), 401
-
-
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
