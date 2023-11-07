7cdaad549e12df3b928f1e184574b21726d9e08e
AbiollaghJames <abiollajames@protonmail.com>
2023-11-07 08:01:30 +0300

    Basic Authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ Basic Auth module
 """
 
 from .auth import Auth
+from typing import TypeVar
 import base64
 
 
@@ -56,7 +57,9 @@ class BasicAuth(Auth):
         u_name, passwd = decoded_base64_authorization_header.split(':', 1)
         return (u_name, passwd)
 
-    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
+    def user_object_from_credentials(
+        self, user_email: str, user_pwd: str
+    ) -> TypeVar('User'):
         """
         returns the User instance based on his email and password
         """
