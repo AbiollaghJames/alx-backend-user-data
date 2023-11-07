62a05202d0782a041755db38779edb2736ef3a69
AbiollaghJames <abiollajames@protonmail.com>
2023-11-07 07:22:56 +0300

    Basic Authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -16,16 +16,12 @@ class Auth:
         """
         check string params if they need auth
         """
-        if path is None:
-            return True
-
-        if excluded_paths is None or not excluded_paths:
+        if path is None or excluded_paths is None or not excluded_paths:
             return True
 
         for excluded_path in excluded_paths:
             if fnmatch.fnmatch(path, excluded_path):
                 return False
-
         return True
 
     def authorization_header(self, request=None) -> str:
