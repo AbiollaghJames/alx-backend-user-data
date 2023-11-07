cd28950e45568104c6ac6006173160615a21764b
AbiollaghJames <abiollajames@protonmail.com>
2023-11-07 07:45:06 +0300

    Basic Authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -41,3 +41,19 @@ class BasicAuth(Auth):
         except Exception:
             return None
         return decoded_text
+
+    def extract_user_credentials(
+        self, decoded_base64_authorization_header: str
+    ) -> (str, str):
+        """
+        returns the user email and password
+        from the Base64 decoded value.
+        """
+        if decode_base64_authorization_header is None or not isinstance(
+            decoded_base64_authorization_header, str
+        ):
+            return (None, None)
+        if ':' not in decoded_base64_authorization_header:
+            return (None, None)
+        u_name, passwd = decoded_base64_authorization_header.split(':', 1)
+        return (u_name, passwd)
