71f07faa41c440fdc0946db8cead584bee74de0a
AbiollaghJames <abiollajames@protonmail.com>
2023-11-07 06:54:41 +0300

    Basic Authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,10 +4,19 @@ Basic Auth module
 """
 
 from .auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """
     Implementation of Basic Auth
     """
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+        returns the Base64 part of the Authorization header
+        """
+        if authorization_header is None or not isinstance(authorization_header, str):
+            return None
+        if 'Basic' not in authorization_header:
+            return None
+        return authorization_header[6:]
