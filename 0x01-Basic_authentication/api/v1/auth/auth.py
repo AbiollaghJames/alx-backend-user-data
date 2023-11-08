32da18d978f37d87fb9827fcb96044c4a3b12f36
AbiollaghJames <abiollajames@protonmail.com>
2023-11-08 12:51:27 +0300

    Basic Authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,7 +5,6 @@ Authentication module
 
 from flask import request
 from typing import List, TypeVar
-import fnmatch
 
 
 class Auth:
@@ -19,19 +18,21 @@ class Auth:
         if path is None or excluded_paths is None or not excluded_paths:
             return True
 
+        path = path.rstrip("/")
         for excluded_path in excluded_paths:
-            if fnmatch.fnmatch(path, excluded_path):
+            if excluded_path.endswith("*") and path.startswith(excluded_path[:-1]):
                 return False
-
+            elif path == excluded_path.rstrip("/"):
+                return False
         return True
 
     def authorization_header(self, request=None) -> str:
         """
         Holds the header information
         """
-        if request is None or 'Authorization' not in request.headers:
-            return None
-        return request.headers.get('Authorization')
+        if request is not None:
+            return request.headers.get('Authorization', None)
+        return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
