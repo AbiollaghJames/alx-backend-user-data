356898b44f860210a6bc72d123ddf3d56688b46d
AbiollaghJames <abiollajames@protonmail.com>
2023-11-09 07:02:56 +0300

    Session Authentication

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -30,4 +30,4 @@ class SessionAuth(Auth):
         """
         if session_id is None or not isinstance(session_id, str):
             return None
-        return self.user_id_by_session_id.get('session_id', None)
+        return self.user_id_by_session_id.get(session_id, None)
