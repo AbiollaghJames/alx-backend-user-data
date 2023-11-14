d438e212cb9d256220c788de215775a725996c16
AbiollaghJames <abiollajames@protonmail.com>
2023-11-14 18:43:11 +0300

    Flask User Authentication

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,14 +2,28 @@
 """Flask app module
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"])
 def home():
     return jsonify({"message": "Bienvenue"})
+
+@app.route("/users", methods=["POST"])
+def users():
+    """Register user
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    return jsonify({"email": "{email}", "message": "user created"})
 
 
 if __name__ == "__main__":
