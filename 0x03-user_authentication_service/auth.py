8bd04c34a6c1564718a44c20cb9dab5e487c289a
AbiollaghJames <abiollajames@protonmail.com>
2023-11-15 08:06:21 +0300

    User Authentication

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -76,3 +76,9 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroy session by updating session id to None
+        """
+        user = self._db.update_user(user_id, session_id=None)
+        return None
