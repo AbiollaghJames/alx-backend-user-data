48fe3b4f2c5300f1b35d2688fa1f054b663669e2
AbiollaghJames <abiollajames@protonmail.com>
2023-11-15 08:39:16 +0300

    User Authentication

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -47,13 +47,11 @@ def logout():
     """Logs the user out
     """
     session_id = request.cookies.get("session_id")
-    if session_id is None:
-        abort(403)
     user = AUTH.get_user_from_session_id(session_id)
-    if user is None:
+    if session_id is None or user is None:
         abort(403)
     AUTH.destroy_session(user_id)
-    return redirect(url_for("home"))
+    return redirect("/")
 
 
 @app.route("/profile", methods=["GET"])
