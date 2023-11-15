922975ac78085dfa79c18e94d4b33c2effa01122
AbiollaghJames <abiollajames@protonmail.com>
2023-11-15 09:00:11 +0300

    User Authentication

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -65,5 +65,17 @@ def profile():
     abort(403)
 
 
+@app.route("/reset_password", methods=["POST"])
+def get_reset_password_token():
+    """Resets password
+    """
+    email = request.form.get("email")
+    try:
+        generated_tok = AUTH.get_reset_password_token(email)
+        return jsonify({"email": f"{email}", "reset_token": f"{generated_tok}"}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
